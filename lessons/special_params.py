7ff2306350e62ace9502f978997f372700d1a3e1
Sawyer Grizzard <sawyerg@live.unc.edu>
2021-11-15 13:10:33 -0500

    Progress on Lesson 23

--- lessons/special_params.py
+++ lessons/special_params.py
@@ -1,10 +1,17 @@
 """Examples of optional parameters and Union type parameters."""
 
+from typing import Union
 
-def hello(name: str = "World") -> str:
+
+def hello(name: Union[str, int, float] = "World") -> str:
     """A delightful greeting function."""
     result: str = "Hello, "
-    return result + name
+    if isinstance(name, str):
+        return result + name
+    elif isinstance(name, float): 
+        return result + "alien from sector " + str(name)
+    else:
+        return result + "COMP" + str(name)
 
 
 # Calling hello with no arguments leads to use of default value
@@ -12,3 +19,4 @@ print(hello())
 # Calling hello with one argument overrides the default value
 print(hello("Ezekiel"))
 print(hello(110))
+print(hello(3.14))
