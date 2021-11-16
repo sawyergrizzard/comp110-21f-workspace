814a93c5af2b10002efe25a1d3edfe9df2c326b9
Sawyer Grizzard <sawyerg@live.unc.edu>
2021-11-16 18:30:59 -0500

    Progress on EX08

--- exercises/ex08/Simpy.py
+++ exercises/ex08/Simpy.py
@@ -4,10 +4,44 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730245854"
 
 
 class Simpy:
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float]):
+        """Initialize a new Simpy object."""
+        self.values = values
+
+    def __str__(self) -> str:
+        """String representation of a Simpy."""
+        return f"Simpy({self.values})"
+
+    def fill(self, value: float, times: int) -> None:
+        """Fill our values array by mutating object called on."""
+        # Start with an empty values list
+        self.values = []
+        # Loop for 'times' number of times
+        i: int = 0
+        while i < times:
+        #   append value parameter to the values array
+            self.values.append
+            i += 1
+
+    def arange(self, start: float, stop: float, step: float = 1.0) -> None:
+        """Fill in a range of values."""
+        # Start an empty values list
+        self.values = []
+        # Be sure step is not 0.0 by asserting
+        assert step != 0.0
+        # When step is positive:
+        if step > 0.0:
+            # Initialize next value to start
+            next_value: float = start 
+            # While next value is less than stop value
+            while next_value < stop: 
+                # Add next value to values list
+                self.values.append(next_value)
+                # Update next value by adding the step to it
+                next_value += step
