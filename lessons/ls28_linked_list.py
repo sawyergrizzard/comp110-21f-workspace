f69916364a1de3b816f758c9bf224df0cc60a285
Sawyer Grizzard <sawyerg@live.unc.edu>
2021-12-01 15:02:36 -0500

    Progress on Exercise 09

--- lessons/ls28_linked_list.py
+++ lessons/ls28_linked_list.py
@@ -16,7 +16,15 @@ class Node:
         self.next = next
 
 
+def count(head: Optional[Node]) -> int:
+    """Counts up the length of a linked list."""
+    if head is None:
+        return 0
+    else:
+        return 1 + count(head.next)
+
+
 third_node: Node = Node(3, None)
 second_node: Node = Node(2, third_node)
-a_node: Node = Node(1, second_node)
-print(a_node.data.next)
+a_node: Node = Node(0, second_node)
+print(count(a_node))
